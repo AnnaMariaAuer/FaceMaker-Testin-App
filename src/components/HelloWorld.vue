dbc85293371a372616179e8d580a49640cb047b4
Anna <annamaria.auer0706@gmail.com>
2019-09-14 09:47:54 +0200

    unique name per json-file

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -840,7 +840,7 @@ export default {
           const blob = new Blob([data], {type: 'text/plain'})
           const e = document.createEvent('MouseEvents'),
           a = document.createElement('a');
-          a.download = "test.json";
+          a.download = "user_" + this.user_id.toString() + "_ratings.json";
           a.href = window.URL.createObjectURL(blob);
           a.dataset.downloadurl = ['text/json', a.download, a.href].join(':');
           e.initEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
